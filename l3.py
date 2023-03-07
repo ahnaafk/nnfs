26e73f7e0b10a0fa487c75c4efca505040bd60ed
Ahnaaf Khan <ahnaafk@gmail.com>
2023-03-07 10:58:25 -0500

    Updated matrix multiplication

--- l3.py
+++ l3.py
@@ -1,9 +1,18 @@
 import numpy as np
 
-a = [1, 2, 3]
-b = [2, 3, 4]
+#shape of (3,4)
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
-a = np.array([a])
-b = np.array([b]).T
+#shape of (3,4)
+weights = [[0.2, 0.8, -0.5, 1.0], 
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-print(np.dot(a,b))
+bias = [2.0, 3.0, 0.5]
+
+#transpose weights to be of shape (4,3)
+layer_outputs = np.dot(inputs, np.array(weights).T) + bias
+
+print(layer_outputs)
