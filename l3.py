fc2151d560f1c5a23b4c722f1e6ba03831165c28
Ahnaaf Khan <ahnaafk@gmail.com>
2023-03-07 19:18:08 -0500

    Add 2nd layer

--- l3.py
+++ l3.py
@@ -12,7 +12,17 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 bias = [2.0, 3.0, 0.5]
 
-#transpose weights to be of shape (4,3)
-layer_outputs = np.dot(inputs, np.array(weights).T) + bias
+#shape of (n,3) -> must have 3 columns as there are only 3 inputs. 
+# can have as many weight sets (neurons) though. 
+weights2 = [[0.1, -0.14, 0.5], 
+           [-0.5, 0.12, -0.33],
+           [-0.44, 0.73, -0.13]]
 
-print(layer_outputs)
+bias2 = [-1, 2, 0.5]
+
+#transpose weights to be of shape (4,3)
+layer_outputs1 = np.dot(inputs, np.array(weights).T) + bias
+layer_outputs2 = np.dot(layer_outputs1, np.array(weights2).T) + bias2
+
+print(layer_outputs1)
+print(layer_outputs2)
