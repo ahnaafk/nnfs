3d77445be88e3dc880f859c7be81067222231889
Ahnaaf Khan <ahnaafk@gmail.com>
2023-03-14 13:20:11 -0400

    Add Softmax

--- l4.py
+++ l4.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 #dense layer
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons) -> None:
+    def __init__(self, n_inputs, n_neurons):
         #initialize weights and biases
         self.weights = 0.01 * np.random.rand(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
@@ -17,9 +17,32 @@ class Layer_Dense:
         #calculate output values from inputs * weights + biases
         self.output = np.dot(inputs, self.weights) + self.biases
 
-X, y = spiral_data(samples=100, classes=3)
-dense1 = Layer_Dense(2,3)
-dense1.forward(X)
+class Activation_ReLU:
+    
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-print(dense1.output[:5])
-print(dense1.output.shape)
+X, y = spiral_data(samples=100, classes=3)
+
+class Activation_Softmax:
+
+    def forward(self, inputs):
+
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output) #forward pass wrapped in activation function
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
